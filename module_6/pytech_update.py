31ef80a9ec1e248a1c1118b3523eb42f33930da0
jaredrobles <119830668+jaredrobles@users.noreply.github.com>
2023-01-29 22:18:36 -0600

    Add files via upload

--- module_6/pytech_update.py
+++ module_6/pytech_update.py
@@ -1,46 +1,25 @@
-""" 
-    Title: pytech_update.py
-    Author: Professor Krasso
-    Date: 10 July 2020
-    Description: Test program for updating a document in the pytech collection
-"""
-
-""" import statements """
 from pymongo import MongoClient
 
-# MongoDB connection string 
-url = "mongodb+srv://admin:admin@cluster0.rsnru.mongodb.net/pytech?retryWrites=true&w=majority"
-
-# connect to the MongoDB cluster 
+url = "mongodb+srv://admin:admin@cluster0.8aucclw.mongodb.net/pytech"
 client = MongoClient(url)
 
-# connect pytech database
-db = client.pytech
-
-# get the students collection 
+db=client.pytech
 students = db.students
 
-# find all students in the collection 
-student_list = students.find({})
+student_list=students.find({})
 
-# display message 
-print("\n  -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
+print("\n -- DISPLAYING STUDENTS DOCUMENTS FROM find() QUERY --")
 
-# loop over the collection and output the results 
 for doc in student_list:
-    print("  Student ID: " + doc["student_id"] + "\n  First Name: " + doc["first_name"] + "\n  Last Name: " + doc["last_name"] + "\n")
+    print(" Student ID: " + doc["student_id"] + "\n First Name: " + doc["first_name"] + "\n Last Name: " + doc["last_name"] + "\n")
+    
 
-# update student_id 1007
-result = students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Oakenshield II"}})
+result = students.update_one({"student_id": "1007"}, {"$set": {"last_name": "Smith"}})
 
-# find the updated student document 
 thorin = students.find_one({"student_id": "1007"})
 
-# display message
-print("\n  -- DISPLAYING STUDENT DOCUMENT 1007 --")
+print(" Student ID: " + thorin["student_id"] + "\n First Name: " + thorin["first_name"] + "\n Last Name: " + thorin["last_name"] + "\n")
 
-# output the updated document to the terminal window
-print("  Student ID: " + thorin["student_id"] + "\n  First Name: " + thorin["first_name"] + "\n  Last Name: " + thorin["last_name"] + "\n")
 
-# exit message 
-input("\n\n  End of program, press any key to continue...")
+input("\n\n Press any key to continue...")
+
